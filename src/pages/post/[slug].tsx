3e1c00fe10afaa790f76370470b71a29139a21cc
mpsantos8 <matheus.masan@gmail.com>
2021-05-03 12:57:40 -0300

    Implement the post screen

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,9 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { RichText } from 'prismic-dom';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
@@ -27,20 +32,80 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post() {
-  return <Header />;
+export default function Post(props: PostProps) {
+  const { post } = props;
+  return (
+    <>
+      <Header />
+      <img className={styles.banner} alt="image" src={post.data.banner.url} />
+      <main className={commonStyles.container}>
+        <article className={styles.post}>
+          <h1>{post.data.title}</h1>
+          <div>
+            <time>
+              <FiCalendar /> {post.first_publication_date}
+            </time>
+            <span>
+              <FiUser /> {post.data.author}
+            </span>
+            <span>
+              <FiClock />
+            </span>
+          </div>
+          {post.data.content.map(section => (
+            <section className={styles.postContent}>
+              <h2>{section.heading}</h2>
+              <div dangerouslySetInnerHTML={{ __html: section.body.text }} />
+            </section>
+          ))}
+        </article>
+      </main>
+    </>
+  );
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  // const posts = await prismic.query(TODO);
 
-//   // TODO
-// };
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async context => {
+  const prismic = getPrismicClient();
+  const { slug } = context.params;
 
-//   // TODO
-// };
+  const response = await prismic.getByUID('post', String(slug), {});
+
+  const content = response.data.content.map(section => ({
+    heading: RichText.asText(section.heading),
+    body: {
+      text: RichText.asHtml(section.body),
+    },
+  }));
+
+  const post = {
+    first_publication_date: format(
+      new Date(response.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+    data: {
+      title: RichText.asText(response.data.title),
+      banner: {
+        url: response.data.banner.url,
+      },
+      author: RichText.asText(response.data.author),
+      content,
+    },
+  };
+
+  return {
+    props: { post },
+  };
+};
